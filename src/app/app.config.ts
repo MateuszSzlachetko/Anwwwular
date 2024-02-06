5878503d857f719a77aeed16a01497e29ab96b0f
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2024-02-06 14:12:55 +0100

    feat: add image loader and pages folder

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { IMAGE_LOADER, ImageLoaderConfig } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    {
+      provide: IMAGE_LOADER,
+      useValue: (config: ImageLoaderConfig) => {
+        if (config.src.endsWith('webp') && config.width) {
+          return `${config.src.slice(0, -5)}${config.width}.webp`;
+        }
+        return `${config.src}`;
+      },
+    },
+  ],
 };
